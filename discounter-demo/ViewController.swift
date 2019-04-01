1c5ea2fe18efa6dc96e32d9d87d5accf67e762b9
Mohammad Ashour <ashour.mohammad@gmail.com>
2019-04-01 14:19:14 +0100

    Initial Commit

---
+++ discounter-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  discounter-demo
+//
+//  Created by Mohammad Ashour on 2019-04-01.
+//  Copyright © 2019 Mohammad Ashour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
