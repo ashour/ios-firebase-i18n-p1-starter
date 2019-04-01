a35eee8f12a0ff13ee7a538f35e5cf178575c223
Mohammad Ashour <ashour.mohammad@gmail.com>
2019-04-01 18:05:25 +0100

    Initial Commit: Basic tab bar navigation, customized tab bar, and screen placeholders

--- discounter-demo/Controllers/FeedViewController.swift
+++ discounter-demo/Controllers/FeedViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ discounter-demo/Views/DSCTabBarItem.swift
@@ -0,0 +1,26 @@
+//
+//  DSCTabBarItem.swift
+//  discounter-demo
+//
+//  Created by Mohammad Ashour on 2019-04-01.
+//  Copyright © 2019 Mohammad Ashour. All rights reserved.
+//
+
+import UIKit
+
+class DSCTabBarItem: UITabBarItem {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        setCustomFont()
+    }
+    
+    fileprivate func setCustomFont() {
+        let textAttributes: [NSAttributedString.Key: Any] =
+            [NSAttributedString.Key.font: UIFont(name: "NotoSans-Medium", size: 10)!]
+        
+        
+        setTitleTextAttributes(textAttributes, for: .normal)
+    }
+}
